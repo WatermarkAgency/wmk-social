c5f28ae9c95268b22e12449686d78f095472e852
Peter Washington <peter@wmkagency.com>
2022-05-18 15:28:16 -0600

    Social media Icon links helper converted to TS/package

--- src/SocialMediaIcons/SocialMediaIcon.tsx
+++ src/SocialMediaIcons/SocialMediaIcon.tsx
@@ -1,37 +1,31 @@
 import * as React from "react";
-import { Anchor } from "../Anchor";
-import PropTypes from "prop-types";
+import { LinkTarget, WmkLink } from "wmk-link";
 
-const SocialMediaIcon = React.forwardRef(
-  ({ platform, url, target, className }, ref) => {
+interface SocialsProps {
+  platform: { Icon: JSX.Element; name: string };
+  url: string;
+  target?: LinkTarget;
+  className?: string;
+}
+
+const SocialMediaIcon = React.forwardRef<HTMLDivElement, SocialsProps>(
+  ({ platform, url, target = "blank", className = "" }: SocialsProps, ref) => {
     const { Icon, name } = platform;
     const _className = name + (className !== "" ? " " + className : "");
     return (
-      <Anchor
+      <WmkLink
         key={name}
         to={url}
         target={target}
         className={_className}
         ref={ref}>
-        <Icon />
+        {Icon}
         <span className="sr-only sr-only-focusable">
           {"Open " + name + " page"}
         </span>
-      </Anchor>
+      </WmkLink>
     );
   }
 );
 
 export default SocialMediaIcon;
-
-SocialMediaIcon.propTypes = {
-  platform: PropTypes.object.isRequired,
-  url: PropTypes.string.isRequired,
-  target: PropTypes.string,
-  className: PropTypes.string
-};
-
-SocialMediaIcon.defaultProps = {
-  target: "_blank",
-  className: ""
-};
--- src/SocialMediaIcons/SocialMediaIcons.tsx
+++ src/SocialMediaIcons/SocialMediaIcons.tsx
@@ -1,53 +1,86 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import SocialMediaIcon from "./SocialMediaIcon";
 import { wmkClass } from "../logic";
+import { LinkTarget } from "wmk-link";
+import {
+  GrFacebook,
+  GrInstagram,
+  GrLinkedin,
+  GrMedium,
+  GrPinterest,
+  GrReddit,
+  GrSnapchat,
+  GrSpotify,
+  GrTumblr,
+  GrTwitter,
+  GrVimeo,
+  GrYoutube
+} from "react-icons/gr";
+import { FaBehance, FaFlickr, FaHouzz, FaTiktok, FaYelp } from "react-icons/fa";
 
-export const SocialMediaIcons = ({ query, platforms, className }) => {
-  const getPlatform = (platform, platforms) => {
-    let socialPlatform = null;
-    platforms.forEach((p) => {
-      if (platform.toLowerCase().indexOf(p.name) !== -1) {
-        socialPlatform = p;
-      }
-    });
-    return socialPlatform;
+export interface SocialIconQuery {
+  title: string;
+  url: string;
+  target?: LinkTarget;
+}
+
+export interface SocialPlatformFields {
+  name: string;
+}
+
+export const defaultPlatforms = {
+  facebook: <GrFacebook />,
+  twitter: <GrTwitter />,
+  linkedin: <GrLinkedin />,
+  youtube: <GrYoutube />,
+  instagram: <GrInstagram />,
+  pinterest: <GrPinterest />,
+  yelp: <FaYelp />,
+  medium: <GrMedium />,
+  reddit: <GrReddit />,
+  snapchat: <GrSnapchat />,
+  spotify: <GrSpotify />,
+  tumblr: <GrTumblr />,
+  vimeo: <GrVimeo />,
+  tiktok: <FaTiktok />,
+  behance: <FaBehance />,
+  flickr: <FaFlickr />,
+  houzz: <FaHouzz />
+};
+
+export const SocialMediaIcons = ({
+  socials,
+  platforms = defaultPlatforms,
+  className = ""
+}: {
+  socials: SocialIconQuery[];
+  platforms?: { [key: string]: JSX.Element };
+  className?: string;
+}) => {
+  const getPlatform = (platform: string) => {
+    const p = platform.toLocaleLowerCase();
+    return p in platforms ? (
+      platforms[p]
+    ) : (
+      <div>Error: No Icon in platform definition for {p}</div>
+    );
   };
   const _className = wmkClass("icon", "social", className);
   return (
     <div>
-      {query.map((icon) => {
+      {socials.map((icon) => {
         const { title, url, target } = icon;
-        const platform = getPlatform(title, platforms);
+        const platform = getPlatform(title);
         return (
           <SocialMediaIcon
-            platform={platform}
+            platform={{ name: title, Icon: platform }}
             url={url}
             target={target}
             className={_className}
-            key={platform.name + "-icon"}
+            key={title + "-icon"}
           />
         );
       })}
     </div>
   );
-};
-
-SocialMediaIcons.propTypes = {
-  query: PropTypes.array.isRequired,
-  platforms: PropTypes.arrayOf([PropTypes.object]),
-  className: PropTypes.string,
-};
-
-SocialMediaIcons.defaultProps = {
-  platforms: [
-    { name: "facebook", Icon: () => <em>error</em> },
-    { name: "twitter", Icon: () => <em>error</em> },
-    { name: "linkedin", Icon: () => <em>error</em> },
-    { name: "youtube", Icon: () => <em>error</em> },
-    { name: "instagram", Icon: () => <em>error</em> },
-    { name: "pinterest", Icon: () => <em>error</em> },
-    { name: "yelp", Icon: () => <em>error</em> },
-  ],
-  className: "",
 };
